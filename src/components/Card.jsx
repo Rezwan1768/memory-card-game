4753e96603f483908ccd676999e12ff60d619776
Rezwan <rezwan1768@gmail.com>
2025-06-13 04:14:06 -0400

    Create card styles for each pokemon type

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
+import "../styles/card.css";
 
 export function Card() {
   const [pokemonInfo, setPokemonInfo] = useState({ name: "", image: "" });
-  const URL = "https://pokeapi.co/api/v2/pokemon/890";
+  const URL = "https://pokeapi.co/api/v2/pokemon/1025";
 
   useEffect(() => {
     fetch(URL)
@@ -20,22 +21,52 @@ export function Card() {
           name,
           image,
         });
-        console.log(data.sprites);
+        console.log(data);
       });
   }, []);
 
+  const types = [
+    "normal",
+    "fighting",
+    "fire",
+    "ice",
+    "water",
+    "flying",
+    "dragon",
+    "bug",
+    "grass",
+    "rock",
+    "ground",
+    "steel",
+    "psychic",
+    "fairy",
+    "electric",
+    "ghost",
+    "poison",
+    "dark",
+    "default",
+  ];
   return (
-    <div>
-      {/* Only render <img> when the image URL is available to avoid empty src warning */}
-      {pokemonInfo.image && (
-        <img
-          src={pokemonInfo.image}
-          alt={pokemonInfo.name}
-          height={200}
-          width={200}
-        />
-      )}
-      <p>{pokemonInfo.name}</p>
+    <div className="container">
+      {/* Testing out different backgrounds for the cards, 
+     thinking of having a different background for each type  */}
+      {types.map((element, index) => {
+        const classes = "card type-" + element;
+        return (
+          <div className={classes} key={index}>
+            {/* Only render <img> when the image URL is available to avoid empty src warning */}
+            {pokemonInfo.image && (
+              <img
+                src={pokemonInfo.image}
+                alt={pokemonInfo.name}
+                height={200}
+                width={200}
+              />
+            )}
+            <p>{pokemonInfo.name}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
